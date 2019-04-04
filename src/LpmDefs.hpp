d1ae6bc6f3c29174e719dda9d079548872419b15
Peter Bosler <pabosle@sandia.gov>
2019-04-04 15:57:06 -0600

    tests pass on cpu & gpu

--- src/LpmDefs.hpp
+++ src/LpmDefs.hpp
@@ -1,10 +1,61 @@
 #ifndef LPM_TYPEDEFS_HPP
 #define LPM_TYPEDEFS_HPP
 
+#include <cmath>
+#include <cassert>
+#include <iostream>
+#include <sstream>
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <algorithm>
 #include "LpmConfig.h"
 #include "Kokkos_Core.hpp"
 
+#ifdef HAVE_CUDA
+#define KOKKOS_CONSTANT __constant__ __device__
+#else
+#define KOKKOS_CONSTANT
+#endif
+
 namespace Lpm {
+namespace ko = Kokkos;
+/// print message
+#define pr(m) do {                              \
+    std::stringstream _ss_;                     \
+    _ss_ << m << std::endl;                     \
+    std::cerr << _ss_.str();                    \
+  } while (0)
+#define prc(m) pr(#m << " | " << (m))
+#define puf(m)"(" << #m << " " << (m) << ")"
+#define pu(m) << " " << puf(m)
+template<typename T>
+static void prarr (const std::string& name, const T* const v, const size_t n) {
+  std::cerr << name << ": ";
+  for (size_t i = 0; i < n; ++i) std::cerr << " " << v[i];
+  std::cerr << "\n";
+}
+
+/// Error handling
+#define LPM_THROW_IF(condition, message) do { \
+    if (condition) {                                                    \
+      std::stringstream _ss_;                                           \
+      _ss_ << __FILE__ << ":" << __LINE__ << ": The condition:\n" << #condition \
+        "\nled to the exception\n" << message << "\n";                  \
+      throw std::logic_error(_ss_.str());                               \
+    }                                                                   \
+} while (0)
+
+#define LPM_STDERR_IF(condition, message) do { \
+  try { LPM_THROW_IF(condition, message); } \
+  catch (const std::logic_error& e) { std::cerr << e.what(); } \
+} while (0)
+
+KOKKOS_INLINE_FUNCTION static void error (const char* const msg)
+{ ko::abort(msg); }
+
+KOKKOS_INLINE_FUNCTION static void message (const char* const msg)
+{ printf("%s\n", msg); }
 
 /// Real number type
 typedef double Real;
@@ -12,34 +63,134 @@ typedef double Real;
 typedef int Int;
 /// Memory index type
 typedef int Index;
+/// Memory layout
+#ifdef HAVE_CUDA
+typedef ko::LayoutLeft Layout;
+#else
+typedef ko::LayoutRight Layout;
+#endif
 
-typedef Kokkos::View<Real*> RealArray;
-typedef Kokkos::View<Index*> IndexArray;
-typedef Kokkos::View<const Index*> ConstIndexArray;
+/// Lpm array types
+template <int ndim, typename ExeSpace> using Vec = ko::View<Real[ndim], Layout, ExeSpace>;
+template <int ndim, typename ExeSpace> using VecArr = ko::View<Real*[ndim], Layout, ExeSpace>;
+template <int ndim> using ConstVecArr = ko::View<const Real*[ndim], Layout>;
+typedef ko::View<Index*, Layout> Idxs;
+typedef ko::View<const Index*, Layout> ConstIdxs;
 
+/// choose host or device version of an array
+template <typename ViewType, typename ExeSpace> struct InExeSpace {
+    typedef ViewType type;
+};
+
+template <typename ViewType> struct InExeSpace<ViewType, ko::HostSpace> {
+    typedef typename ViewType::HostMirror type;
+};
+/// copy array contents
+template<typename ViewType, typename ConstViewType>
+KOKKOS_INLINE_FUNCTION
+static void copy(ViewType dst, ConstViewType src, const Int n) {
+    for (Int i=0; i<n; ++i)
+        dst[i] = src[i];
+}
+
+#ifdef HAVE_CUDA
+/// 1d slice of an array
+template <typename VT> KOKKOS_FORCEINLINE_FUNCTION
+ko::View<typename VT::value_type*, ko::LayoutStride, typename VT::device_type, ko::MemoryTraits<ko::Unmanaged>>
+slice(const VT& v, Int i) {return ko::subview(v, i, ko::ALL());}
+/// explicitly const 1d slice of an array
+template <typename VT> KOKKOS_FORCEINLINE_FUNCTION
+ko::View<typename VT::const_value_type*, ko::LayoutStride, typename VT::device_type, ko::MemoryTraits<ko::Unmanaged>>
+const_slice(const VT& v, Int i) {return ko::subview(v, i, ko::ALL());}
+#else
+template <typename VT> KOKKOS_FORCEINLINE_FUNCTION
+typename VT::value_type*
+slice(const VT& v, Int i) {return v.data() + v.extent(1)*i;}
+
+template <typename VT> KOKKOS_FORCEINLINE_FUNCTION
+typename VT::const_value_type*
+const_slice(const VT& v, Int i) {return v.data() + v.extent(1)*i;}
+#endif
 
 /// Pi
-static const Real PI = 3.1415926535897932384626433832795027975;
+static constexpr Real PI = 3.1415926535897932384626433832795027975;
 /// Radians to degrees conversion factor
-static const Real RAD2DEG = 180.0 / PI;
+static constexpr Real RAD2DEG = 180.0 / PI;
 
 /// Gravitational acceleration
-static const Real G = 9.80616;
+static constexpr Real G = 9.80616;
 
 /// Mean sea level radius of the Earth (meters)
-static const Real EARTH_RADIUS_METERS = 6371220.0;
+static constexpr Real EARTH_RADIUS_METERS = 6371220.0;
 
 /// One sidereal day, in units of seconds
-static const Real SIDEREAL_DAY_SEC = 24.0 * 3600.0;
+static constexpr Real SIDEREAL_DAY_SEC = 24.0 * 3600.0;
 
 /// Rotational rate of Earth about its z-axis
-static const Real EARTH_OMEGA_HZ = 2.0 * PI / SIDEREAL_DAY_SEC;
+static constexpr Real EARTH_OMEGA_HZ = 2.0 * PI / SIDEREAL_DAY_SEC;
 
 /// Floating point zero
-static const Real ZERO_TOL = 1.0e-13;
+static constexpr Real ZERO_TOL = 1.0e-13;
 
 /// Supported geometries
 enum GeometryType {PLANAR_GEOMETRY, SPHERICAL_SURFACE_GEOMETRY};
+
+#ifdef KOKKOS_ENABLE_CUDA
+    /// GPU-friendly replacements for stdlib functions
+    template <typename T> KOKKOS_INLINE_FUNCTION 
+    const T& min (const T& a, const T& b) {return a < b ? a : b;}
+    
+    template <typename T> KOKKOS_INLINE_FUNCTION
+    const T& max (const T& a, const T& b) {return a > b ? a : b;}
+    
+    template <typename T> KOKKOS_INLINE_FUNCTION
+    const T* max_element(const T* const begin, const T* const end) {
+        const T* me = begin;
+        for (const T* it=begin +1; it < end; ++it) {
+            if (!(*it < *me)) me = it;
+        }
+        return me;
+    }
+    
+#else
+    using std::min;
+    using std::max;
+    using std::max_element;
+#endif
+
+/// Timers 
+static timeval tic () {
+  timeval t;
+  gettimeofday(&t, 0);
+  return t;
+}
+static double calc_et (const timeval& t1, const timeval& t2) {
+  static const double us = 1.0e6;
+  return (t2.tv_sec * us + t2.tv_usec - t1.tv_sec * us - t1.tv_usec) / us;
+}
+static double toc (const timeval& t1) {
+  Kokkos::fence();
+  timeval t;
+  gettimeofday(&t, 0);
+  return calc_et(t1, t);
+}
+static double get_memusage () {
+  static const double scale = 1.0 / (1 << 10); // Memory in MB.
+  rusage ru;
+  getrusage(RUSAGE_SELF, &ru);
+  return ru.ru_maxrss*scale;
+}
+static void print_times (const std::string& name, const double* const parts,
+                         const int nparts) {
+  double total = 0; for (int i = 0; i < nparts; ++i) total += parts[i];
+  printf("%20s %1.3e s %7.1f MB", name.c_str(), total, get_memusage());
+  for (int i = 0; i < nparts; ++i) printf(" %1.3e s", parts[i]);
+  printf("\n");
+}
+static void print_times (const std::string& name, const double total) {
+   printf("%20s %1.3e s %5.1f MB\n", name.c_str(), total, get_memusage());
+}
+
 
 }
 #endif
--- tests/LpmGeometryUnitTests.cpp
+++ tests/LpmGeometryUnitTests.cpp
@@ -17,54 +17,63 @@ ko::initialize(argc, argv);
     // allocate a Vec on the device
     Vec<2, ExeSpace> r2device_a("a");
     // allocate a mirror on host
-    Vec<2, ExeSpace>::HostMirror r2h = ko::create_mirror_view(r2device_a);
+    Vec<2, ExeSpace>::HostMirror ha = ko::create_mirror_view(r2device_a);
     // initialize on host
-    r2h[0] = r2a[0];
-    r2h[1] = r2a[1];
+    ha[0] = r2a[0];
+    ha[1] = r2a[1];
     // copy to device
-    ko::deep_copy(r2device_a, r2h);
+    ko::deep_copy(r2device_a, ha);
     
     Vec<2, ExeSpace> r2device_b("b");
-    r2h[0] = r2b[0];
-    r2h[1] = r2b[1];
-    ko::deep_copy(r2device_b, r2h);
+    Vec<2, ExeSpace>::HostMirror hb = ko::create_mirror_view(r2device_b);
+    hb[0] = r2b[0];
+    hb[1] = r2b[1];
+    ko::deep_copy(r2device_b, hb);
     
     Vec<2, ExeSpace> r2device_c("c");
-    r2h[0] = r2c[0];
-    r2h[1] = r2c[1];
-    ko::deep_copy(r2device_c, r2h);
+    Vec<2, ExeSpace>::HostMirror hc = ko::create_mirror_view(r2device_c);
+    hc[0] = r2c[0];
+    hc[1] = r2c[1];
+    ko::deep_copy(r2device_c, hc);
     
     // allocate a scalar on the device and a host mirror
     ko::View<Real,Layout,ExeSpace> scalar_result("res");
     ko::View<Real>::HostMirror host_scalar = ko::create_mirror_view(scalar_result);
     Vec<2, ExeSpace> result("res");
+    
+    
     Vec<2, ExeSpace>::HostMirror host_result = ko::create_mirror_view(result);
     VecArr<2, ExeSpace> vecs("vecs", 3);
+    std::cout << "shape(vecs) = (" << vecs.extent(0) << ", " << vecs.extent(1) << ")" << std::endl;
     
+    std::cout << typeid(vecs).name() << std::endl;
     
     // execute functions on device
     ko::parallel_for(1, KOKKOS_LAMBDA (int i) {
-        scalar_result(0) = PlaneGeometry::triArea(r2device_a, r2device_b, r2device_c);
-        if (scalar_result(0) != 4) error("triArea error");
+        scalar_result(0) = PlaneGeometry::triArea<Vec<2,ExeSpace>>(r2device_a, r2device_b, r2device_c);
+        if (scalar_result(0) != 4) printf("triArea error %f\n", scalar_result(0));
         for (int j=0; j<2; ++j) {
             vecs(0,j) = r2device_a(j);
             vecs(1,j) = r2device_b(j);
             vecs(2,j) = r2device_c(j);
         }
         PlaneGeometry::midpoint(result, slice(vecs, 0), slice(vecs, 1));
-        printf("midpt = (%f,%f)\n", result(0), result(1));
+        if (result(0) != 1.5 || result(1) != 0.5) printf("midpt error (%f,%f)\n", result(0), result(1));
         PlaneGeometry::barycenter(result, vecs, 3);
+        if (result(0) != 2.0/3.0 || result(1) != -3) printf("barycenter error (%f,%f)\n", result(0), result(1));
         scalar_result(0) = PlaneGeometry::distance(slice(vecs, 0), slice(vecs,2));
-        if ( std::abs(scalar_result(0) - 2*std::sqrt(26.0)) > ZERO_TOL) error("distance error");
+        if ( std::abs(scalar_result(0) - 2*std::sqrt(26.0)) > ZERO_TOL) printf("distance error: %f\n", scalar_result(0));
         scalar_result(0) = PlaneGeometry::dot(r2device_a, r2device_b);
         }); 
     // copy results to host
     ko::deep_copy(host_result, result);
     prarr("barycenter", host_result.data(), 2);
+    LPM_THROW_IF( (host_result(0) != 2.0/3.0 || host_result(1) != -3), "barycenter error\n");
     ko::deep_copy(host_scalar, scalar_result);
-    LPM_THROW_IF( host_scalar(0) != 2, "dot product error.");
+    LPM_THROW_IF( host_scalar(0) != 2, "dot product error.\n");
     
 }
+std::cout << "tests pass." << std::endl;
 ko::finalize();
 return 0;
 }
