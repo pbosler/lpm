bef114f4311285a7b05674f5b64d7d9c9e9c48ed
Peter Bosler <pabosle@sandia.gov>
2019-04-16 16:00:30 -0600

    .

--- src/LpmPolyMesh2d.cpp
+++ src/LpmPolyMesh2d.cpp
@@ -2,6 +2,35 @@
 
 namespace Lpm {
 
+template <typename FaceType, typename SeedType>
+PolyMesh2d<FaceType,SeedType>::PolyMesh2d(const int initTreeDepth, const int maxTreeDepth )  : 
+    nVerts("nverts"), nFaces("nfaces"), nEdges("nedges"), nLeafEdges("nleafedges"), nLeafFaces("nleaffaces")
+{
+    Index maxverts, maxedges, maxfaces;
+    MeshSeed<SeedType> seed;
+    seed.setMaxAllocations(maxverts, maxedges, maxfaces, maxTreeDepth);
+    
+    vertCrds = crd_view_type("vertcrds", maxverts);
+    lagVertCrds = crd_view_type("lagvertcrds", maxverts);
+    
+    faceCrds = crd_view_type("facecrds", maxfaces);
+    lagFaceCrds = crd_view_type("lagfacecrds", maxfaces);
+    
+    edges = edge_view_type("edges",maxedges);
+    edgeTree = edge_tree_type("edge_tree",maxedges);
+    
+    faceVerts = face_view_type("faceverts", maxfaces);
+    faceEdges = face_view_type("faceedges", maxfaces);
+    faceTree = face_tree_type("face_tree", maxfaces); 
+    
+    ko::parallel_for(1,KOKKOS_LAMBDA (int i) {
+        nVerts(0) = 0;
+        nFaces(0) = 0;
+        nEdges(0) = 0;
+        nLeafEdges(0) = 0;
+        nLeafFaces(0) = 0;
+    });
+}
 
 /// ETI
 template class PolyMesh2d<TriFace, TriHexSeed>;
--- src/LpmPolyMesh2d.hpp
+++ src/LpmPolyMesh2d.hpp
@@ -5,38 +5,52 @@
 #include "LpmDefs.hpp"
 #include "LpmUtilities.hpp"
 #include "LpmGeometry.hpp"
-#include "LpmCoords.hpp"
-#include "LpmEdges.hpp"
-#include "LpmFaces.hpp"
+#include "LpmMeshSeed.hpp"
 
 #include "Kokkos_Core.hpp"
 #include "Kokkos_View.hpp"
 
 namespace Lpm {
 
+struct TriFace {
+    static constexpr Int nverts = 3;
+};
+
+struct QuadFace {
+    static constexpr Int nverts = 4;
+};
+
 template <typename FaceType, typename SeedType> class PolyMesh2d {
     public:
         typedef typename SeedType::geo geo;
-        typedef ko::View<Real[geo::ndim], Dev> crd_view;
-        typedef ko::View<Real[geo::ndim], Host> host_crd_view;
-        typedef ko::View<const Real[geo::ndim],Dev> const_crd_view;
-        typedef ko::View<const Real[geo::ndim],Host> host_const_crd_view;
+        typedef ko::View<Real*[geo::ndim],Dev> crd_view_type; // view(i,:) = position vector of particle i
+        typedef ko::View<Index,Dev> n_view_type;        // view(0) = n
+        typedef ko::View<Index*[4],Dev> edge_view_type; // view(i,:) = (orig, dest, left, right)
+        typedef ko::View<Index*[3],Dev> edge_tree_type; // view(i,:) = (parent, kid0, kid1)
+        typedef ko::View<Index*[FaceType::nverts],Dev> face_view_type; // view(i,:) = (vert0, vert1, vert2...)
+        typedef ko::View<Index*[5],Dev> face_tree_type; // view(i,:) = (parent, kid0, kid1, kid2, kid3)
         
-        KOKKOS_INLINE_FUNCTION
-        Index faceContainingPoint(const const_crd_view crd) const;
+        PolyMesh2d(const int initTreeDepth, const int maxTreeDepth);
         
-        /// Host function
-        Index faceContainingPointHost(const host_const_crd_view crd) const;
+        virtual ~PolyMesh2d() {}
     
     protected:
-        Coords<geo> _vertcrds;
-        Coords<geo> _vertlagcrds;
-        Coords<geo> _facecrds;
-        Coords<geo> _facelagcrds;
+        crd_view_type vertCrds;
+        crd_view_type lagVertCrds;
+        crd_view_type faceCrds;
+        crd_view_type lagFaceCrds;
+        n_view_type nVerts;
+        n_view_type nFaces;
         
-        Edges _edges;
+        edge_view_type edges;
+        edge_tree_type edgeTree;
+        n_view_type nEdges;
+        n_view_type nLeafEdges;
         
-        Faces<FaceType> _faces;
+        face_view_type faceVerts;
+        face_view_type faceEdges;
+        face_tree_type faceTree;
+        n_view_type nLeafFaces;
 };
 
 }
