40cd2b92a6f908474220f13cbababe44007af76a
Peter Bosler <pabosle@sandia.gov>
2019-05-01 16:27:27 -0600

    kernel test correctness verified on cpu/gpu. likely bug in BVE kernels += operator.

--- tests/LpmKernelTest.cpp
+++ tests/LpmKernelTest.cpp
@@ -16,12 +16,11 @@ namespace Kokkos {
     template <int ndim> struct RVec {
         Lpm::Real v[ndim];
         KOKKOS_FORCEINLINE_FUNCTION RVec () {for (int i=0; i<ndim; ++i) v[i] = 0;}
-        KOKKOS_FORCEINLINE_FUNCTION RVec operator += (const RVec<ndim>& o) const {
-            RVec<ndim> result;
+        KOKKOS_FORCEINLINE_FUNCTION RVec operator += (const RVec<ndim>& o) {
             for (int i=0; i<ndim; ++i) {
-                result.v[i] = v[i] + o.v[i];
+                v[i] += o.v[i];
             }
-            return result;
+            return *this;
         }
         KOKKOS_FORCEINLINE_FUNCTION Real& operator [] (const Int i) {return v[i];}
         KOKKOS_FORCEINLINE_FUNCTION const Real& operator [] (const Int i) const {return v[i];}
@@ -30,6 +29,22 @@ namespace Kokkos {
     template <> struct reduction_identity<RVec<3>> {
         KOKKOS_FORCEINLINE_FUNCTION static RVec<3> sum() {return RVec<3>();}
     };
+}
+
+template <typename CV> KOKKOS_INLINE_FUNCTION
+static void cross(ko::RVec<3>& c, const CV a, const CV b) {
+        c.v[0] = a[1]*b[2] - a[2]*b[1];
+        c.v[1] = a[2]*b[0] - a[0]*b[2];
+        c.v[2] = a[0]*b[1] - a[1]*b[0];
+}
+
+template <typename CV> KOKKOS_INLINE_FUNCTION
+static ko::RVec<3> cross(const CV a, const CV b) {
+    ko::RVec<3> c;
+    c.v[0] = a[1]*b[2] - a[2]*b[1];
+    c.v[1] = a[2]*b[0] - a[0]*b[2];
+    c.v[2] = a[0]*b[1] - a[1]*b[0];
+    return c;
 }
 
 struct VecReducer {
@@ -62,11 +77,12 @@ struct VecReducer {
 //         printf("\ti=%d, j=%d\n", i,j);
         auto xs = ko::subview(xx, j, ko::ALL());
         auto xt = ko::subview(x, i, ko::ALL());
-        ko::RVec<3> cp;
-        SphereGeometry::cross(cp.v, xs, xt);
+//         printf("xt = %f, %f, %f\n", xt(0), xt(1), xt(2));
+        const ko::RVec<3> cp = cross(xs, xt);
         for (int k=0; k<3; ++k) {
             v.v[k] += cp.v[k];
         }
+//         printf("cp3 = %f\n", cp.v[2]);
     }
 };
 
@@ -135,21 +151,21 @@ void init(view_type v1, view_type v2, view_type v3) {
     auto h3 = ko::create_mirror_view(v3);
     
     for (int i=0; i<v1.extent(0); ++i) {
-        std::cout << "h1(" << i << ",:) = (";
+//         std::cout << "h1(" << i << ",:) = (";
         for (int j=0; j<3; ++j) {
             h1(i,j) = (j == 0 ? i : (j==1 ? -i : 0));
-            std::cout << h1(i,j) << " ";
+//             std::cout << h1(i,j) << " ";
         }
-        std::cout << ")" << std::endl;
+//         std::cout << ")" << std::endl;
     }
 
     for (int i=0; i<v2.extent(0); ++i) {
-        std::cout << "h2(" << i << ",:) = (";
+//         std::cout << "h2(" << i << ",:) = (";
         for (int j=0; j<3; ++j) {
             h2(i,j) = (j==1 ? i : 0);
-            std::cout << h2(i,j) << " ";
+//             std::cout << h2(i,j) << " ";
         }
-        std::cout << ")" << std::endl;
+//         std::cout << ")" << std::endl;
     }    
     
     for (int i=0; i<v3.extent(0); ++i) {
