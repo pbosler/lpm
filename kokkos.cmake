18db7aab4fc9fa506fcf58e50d5d2c7ef48b8dd1
Peter Bosler <pabosle@sandia.gov>
2019-07-15 21:35:20 -0600

    Build Kokkos as part of Lpm if an external library is not provided.  Need to do same for COmpadre and VTK.

--- kokkos.cmake
+++ kokkos.cmake
@@ -1,18 +1,21 @@
-set(KOKKOS_NEEDED FALSE CACHE LOGICAL "")
+include(ExternalProject)
 
-#https://github.com/E3SM-Project/E3SM/blob/master/components/homme/cmake/Kokkos.cmake
+ExternalProject_Add(
+    Kokkos
+    GIT_REPOSITORY "git@github.com:kokkos/kokkos.git"
+    GIT_TAG "develop"
+    
+    UPDATE_COMMAND ""
+    PATCH_COMMAND ""
+    
+    SOURCE_DIR "${CMAKE_SOURCE_DIR}/tpl/kokkos"
+    CMAKE_ARGS ${KOKKOS_CMAKE_ARGS}
+    
+    TEST_COMMAND ""
+)
 
-if (NOT DEFINED Kokkos_ROOT)
-    set(KOKKOS_NEEDED TRUE)
-    set(KOKKOS_SRC ${CMAKE_SOURCE_DIR}/tpl/kokkos)
-    set(KOKKOS_BUILD_DIR ${CMAKE_BINARY_DIR}/kokkos)
-    set(KOKKOS_INCLUDE_DIR ${KOKKOS_SRC}/core/src ${KOKKOS_BUILD_DIR})
-    set(KOKKOS_LIBRARY_DIR ${KOKKOS_BUILD_DIR})
-    Message(STATUS "Lpm will build Kokkos in ${KOKKOS_BUILD_DIR}")
-else()
-    message(STATUS "Lpm will use the Kokkos library installed at ${Kokkos_ROOT}")
-    set(KOKKOS_INCLUDE_DIR ${Kokkos_ROOT}/include)
-    set(KOKKOS_LIBRARY_DIR ${Kokkos_ROOT}/lib)
-endif()
+set(KOKKOS_INCLUDE_DIRS "${CMAKE_BINARY_DIR}/tpl/kokkos/include")
+set(KOKKOS_LIBRARIES "${CMAKE_BINARY_DIR}/tpl/kokkos/lib/libkokkos.a")
+INCLUDE_DIRECTORIES(${KOKKOS_INCLUDE_DIRS})
 
 
