4bf42b40cdf16d015b3957a3a8462f1ff11ed1ca
Peter Bosler <pabosle@sandia.gov>
2019-07-24 13:58:52 -0600

    kokkos/compadre linking working with external tpl builds.

--- build/openmp/configureLpmOpenMP.sh
+++ build/openmp/configureLpmOpenMP.sh
@@ -2,15 +2,16 @@
 
 rm -rf CMakeFiles/ CMakeCache.txt
 
-SRC_ROOT=$HOME/lpmkokkos
+SRC_ROOT=$HOME/lpm
 EXTRA_ARGS=$1
-export KO=$HOME/kokkos-openmp/gcc-4.8.5
+export KO=$HOME/kokkos-openmp/gcc-6.3.1
+export CP=$HOME/compadre/build/openmp
 export OMPI_CXX=g++
 
 cmake \
 -D CMAKE_BUILD_TYPE:STRING="RelWithDebInfo" \
--D CMAKE_CXX_FLAGS="-g -fopenmp" \
 -D Kokkos_ROOT=$KO \
+-D Compadre_ROOT=$CP \
 -D CMAKE_VERBOSE_MAKEFILE:BOOL=OFF \
 $EXTRA_ARGS \
 $SRC_ROOT
