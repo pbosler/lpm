b290d251933928057ec6aae7eef338bd9dc3a5a0
Peter Bosler <pabosle@sandia.gov>
2020-05-26 11:49:59 -0600

    adding snl laptop.

--- build/serial/configureLpmSerial.sh
+++ build/serial/configureLpmSerial.sh
@@ -14,16 +14,22 @@ elif [ "$HOSTNAME" = "s979562" ] ; then
     export VTK_ROOT=$HOME/installs/vtk-8.1.1
     export KO=$HOME/kokkos-serial-debug
     export CPTK=$HOME/compadre-serial-debug
+elif [ "$HOSTNAME" = "s1046231" ] ; then
+  echo "hostname = $HOSTNAME"
+  SRC_ROOT=$HOME/lpm
+  export VTK_ROOT=/Users/pabosle/vtk-8.2.0/clang-10.0.1
+  export KO=/Users/pabosle/kokkos-develop/serial
+  export CPTK=
 else
     SRC_ROOT=$HOME/lpmkokkos
     export VTK_ROOT=$HOME/VTK-8.1.1
     export KO=$HOME/kokkos-serial
-    export CPTK=$HOME/compadre/build
+    export CPTK=$HOME/compadre/build/serial
 fi
 
 EXTRA_ARGS=$1
 export OMPI_CXX=g++
-# 
+#
 
 
 cmake -Wno-dev \
