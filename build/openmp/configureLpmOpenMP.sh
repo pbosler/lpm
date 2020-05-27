aa7f036f7e34f5a4516b5cac29977ba749bfe1c3
Peter Bosler <pabosle@sandia.gov>
2020-05-27 14:41:57 -0600

    cpu build fixed by removing Compadre temporarily (waiting on its issue #198); shifted kokkos dependence to trilinos dependence.

--- build/openmp/configureLpmOpenMP.sh
+++ build/openmp/configureLpmOpenMP.sh
@@ -2,22 +2,23 @@
 
 rm -rf CMakeFiles/ CMakeCache.txt
 
-SRC_ROOT=$HOME/lpm
+SRC_ROOT=$HOME/aj/lpmkokkos
 EXTRA_ARGS=$1
 if [ "$HOSTNAME" = "s1046231" ]; then
 	export KO=$HOME/kokkos-openmp
 else
-	export KO=$HOME/kokkos-openmp/gcc-6.3.1
+	export KO=$HOME/aj/kokkos-openmp/install
 fi
-export CP=$HOME/compadre/build/openmp
+#export CP=$HOME/aj/compadre/build/openmp/install
 export SP=$HOME/spherepack3.2
 export OMPI_CXX=g++
+export TR=$HOME/aj/trilinos-openmp-debug/install
 
 cmake \
 -D CMAKE_BUILD_TYPE:STRING="DEBUG" \
+-D CMAKE_CXX_FLAGS="-fopenmp -fPIC" \
 -D LPM_ENABLE_DEBUG=ON \
--D Kokkos_ROOT=$KO \
--D Compadre_ROOT=$CP \
+-D Trilinos_ROOT=$TR \
 -D Spherepack_ROOT=$SP \
 -D CMAKE_VERBOSE_MAKEFILE:BOOL=OFF \
 $EXTRA_ARGS \
