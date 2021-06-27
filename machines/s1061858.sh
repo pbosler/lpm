d329791429b3fb713d6c80d3c11b984394e36b6d
Peter Bosler <pabosle@sandia.gov>
2021-06-27 14:15:22 -0600

    vtk linking correctly.

--- machines/s1061858.sh
+++ machines/s1061858.sh
@@ -11,7 +11,7 @@ export NETCDF_INCLUDE_DIR=$NETCDF_ROOT/include
 export NETCDF_LIBRARY_DIR=$NETCDF_ROOT/lib
 export NETCDF_LIBRARY=libnetcdf.a
 
-export VTK_INCLUDE_DIR=$VTK_ROOT/include
+export VTK_INCLUDE_DIR=$VTK_ROOT/include/vtk-8.1
 export VTK_LIBRARY_DIR=$VTK_ROOT/lib
 
 export TRILINOS_INCLUDE_DIR=$TR/include
