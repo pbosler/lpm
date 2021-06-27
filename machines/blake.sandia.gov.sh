b903d91025087da84f3ac64c90cc605de1724a8f
Peter Andrew Bosler <pabosle@blake.sandia.gov>
2021-06-27 15:06:25 -0600

    machine file for blake

---
+++ machines/blake.sandia.gov.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+TR=$HOME/trilinos-lpm
+#TR=$HOME/trilinos-lpm-debug
+
+export HDF5_INCLUDE_DIR=$HDF5_ROOT/include
+export HDF5_LIBRARY_DIR=$HDF5_ROOT/lib
+export HDF5_LIBRARY=libhdf5.a
+export HDF5_HL_LIBRARY=libhdf5_hl.a
+
+export NETCDF_INCLUDE_DIR=$NETCDF_ROOT/include
+export NETCDF_LIBRARY_DIR=$NETCDF_ROOT/lib
+export NETCDF_LIBRARY=libnetcdf.a
+
+export VTK_INCLUDE_DIR=$VTK_ROOT/include/vtk-8.1
+export VTK_LIBRARY_DIR=$VTK_ROOT/lib
+
+export TRILINOS_INCLUDE_DIR=$TR/include
+export TRILINOS_LIBRARY_DIR=$TR/lib
+export TRILINOS_TPL_INCLUDE_DIR=$TR/include
+export TRILINOS_TPL_LIBRARY_DIR=$TR/lib
+
+export YAMLCPP_INCLUDE_DIR=$YAMLCPP_ROOT/include
+export YAMLCPP_LIBRARY_DIR=$YAMLCPP_ROOT/lib
+export YAMLCPP_LIBRARY=libyaml-cpp.a
