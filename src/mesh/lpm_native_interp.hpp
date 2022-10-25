7712778379697dd82755bb3dc587d448830c605a
Peter Bosler <pabosle@sandia.gov>
2022-10-25 10:16:49 -0600

    draft

---
+++ src/mesh/lpm_native_interp.hpp
@@ -0,0 +1,19 @@
+#ifndef LPM_NATIVE_INTERP_HPP
+#define LPM_NATIVE_INTERP_HPP
+
+#include "LpmConfig.h"
+#include "LpmConfig.h"
+#include "lpm_assert.hpp"
+#include "lpm_geometry.hpp"
+#include "lpm_constants.hpp"
+#include "lpm_kokkos_defs.hpp"
+#include "util/lpm_tuple.hpp"
+#include "util/lpm_math.hpp"
+#include "mesh/lpm_polymesh2d.hpp"
+
+namespace Lpm {
+
+
+} // namespace Lpm
+
+#endif
