f5a1b4a201837d77bf6ef0e8fa147f555adc5c81
Peter Bosler <pabosle@sandia.gov>
2023-07-18 21:50:09 -0600

    adding configure scripts for all tpls.

---
+++ scripts/configure_compose_tpl.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# start fresh with CMake
+rm -rf CMake* *.cmake
+
+# Kokkos must have been installed already
+KOKKOS=$HOME/kokkos-debug/install
+
+# get the source (branch lpm-tpl):
+#   git clone git@github.com:pbosler/COMPOSE.git compose
+#   cd compose
+#   git checkout lpm-tpl
+COMPADRE_SRC=$HOME/compadre
+COMPOSE_SRC=$HOME/compose
+
+cmake \
+-DCMAKE_CXX_STANDARD=17 \
+-DBUILD_SHARED_LIBS=OFF \
+-DCMAKE_BUILD_TYPE=DEBUG \
+-DKokkos_DIR=$KOKKOS \
+-DCMAKE_INSTALL_PREFIX=../install-debug \
+$COMPOSE_SRC
