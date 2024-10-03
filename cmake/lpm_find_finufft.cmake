3c0521bd7a2aa534369a5e6954fb3246023ea842
Peter Bosler <peter.bosler@me.com>
2024-10-03 17:20:14 -0600

    cmake debugging for finufft

--- cmake/lpm_find_finufft.cmake
+++ cmake/lpm_find_finufft.cmake
@@ -6,7 +6,7 @@ set(FINUFFT_OPTIONS "FINUFFT_USE_OPENMP ${LPM_USE_OPENMP}")
 CPMAddPackage(
   NAME             Finufft
   GIT_REPOSITORY   https://github.com/flatironinstitute/finufft.git
-  GIT_TAG          master
+  GIT_TAG          v2.3.0
   GIT_SHALLOW      Yes
   GIT_PROGRESS     Yes
   EXCLUDE_FROM_ALL Yes
