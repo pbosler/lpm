3f60891d5469bcc62a826d72498b25b3a143e4d2
Peter Bosler <pabosle@sandia.gov>
2024-12-16 17:38:33 -0700

    separating 1d from 2d pse

---
+++ src/lpm_regularized_kernels_1d.hpp
@@ -0,0 +1,252 @@
+#ifndef LPM_REGULARIZED_KERNELS_HPP
+#define LPM_REGULARIZED_KERNELS_HPP
+
+#include "LpmConfig.h"
+#include "lpm_constants.hpp"
+#include "lpm_geometry.hpp"
+#include "util/lpm_math.hpp"
+#include "util/lpm_tuple.hpp"
+
+namespace Lpm {
+namespace colloc {
+
+namespace impl {
+
+template <typename KernelType> KOKKOS_INLINE_FUNCTION
+Real line_scaled_blob(const Real& x, const KernelType& kernels) {
+  const Real xscaled = x/kernels.eps;
+  return kernels.blob(xscaled)/kernels.eps;
+}
+
+} // namespace impl
+
+/**  Each kernel set provides "blob" functions for interpolation,
+  velocity kernels, and derivatives such as the laplacian, all
+  with the same order.
+*/
+
+struct Line2ndOrder {
+  Real eps;
+  Real epssq;
+  Real denom;
+  static constexpr Int order = 2;
+
+  KOKKOS_INLINE_FUNCTION
+  explicit Line2ndOrder(const Real eps) : eps(eps), epssq(square(eps)),
+    denom(sqrt(constants::PI)) {}
+
+  KOKKOS_INLINE_FUNCTION
+  Real blob(const Real& x) const {
+    return exp(-square(x))/denom;
+  }
+
+  KOKKOS_INLINE_FUNCTION
+  Real scaled_blob(const Real& x) const {
+    return impl::line_scaled_blob(x, *this);
+  }
+
+  KOKKOS_INLINE_FUNCTION
+  Real x_derivative(const Real& x) const {
+    return -2*x*exp(-square(x))/denom;
+  }
+
+  KOKKOS_INLINE_FUNCTION
+  Real laplacian(const Real& x) const {
+    return 4*exp(-square(x))/denom;
+  }
+};
+
+struct Line4thOrder {
+  Real eps;
+  Real epssq;
+  Real denom;
+  static constexpr Int order = 4;
+
+  KOKKOS_INLINE_FUNCTION
+  explicit Line4thOrder(const Real eps) : eps(eps), epssq(square(eps)),
+    denom(sqrt(constants::PI)) {}
+
+  KOKKOS_INLINE_FUNCTION
+  Real blob(const Real& x) const {
+    return (1.5 - square(x))*exp(-square(x))/denom;
+  }
+
+  KOKKOS_INLINE_FUNCTION
+  Real scaled_blob(const Real& x) const {
+    return impl::line_scaled_blob(x, *this);
+  }
+
+  KOKKOS_INLINE_FUNCTION
+  Real x_derivative(const Real& x) const {
+    const Real xsq = square(x);
+    return x * (-5 + 2*xsq)*exp(-xsq)/denom;
+  }
+
+  KOKKOS_INLINE_FUNCTION
+  Real laplacian(const Real& x) const {
+    const Real xsq = square(x);
+    return (10 - 4*xsq)*exp(-xsq)/denom;
+  }
+};
+
+struct Line6thOrder {
+  Real eps;
+  Real epssq;
+  Real denom;
+  static constexpr Int order = 6;
+
+  KOKKOS_INLINE_FUNCTION
+  explicit Line6thOrder(const Real eps) : eps(eps), epssq(square(eps)),
+    denom(sqrt(constants::PI)) {}
+
+  KOKKOS_INLINE_FUNCTION
+  Real blob(const Real& x) const {
+    const Real xsq = square(x);
+    return (15.0/8 - 2.5*xsq + 0.5*square(xsq))*exp(-square(x))/denom;
+  }
+
+  KOKKOS_INLINE_FUNCTION
+  Real scaled_blob(const Real& x) const {
+    return impl::line_scaled_blob(x, *this);
+  }
+
+  KOKKOS_INLINE_FUNCTION
+  Real x_derivative(const Real& x) const {
+    const Real xsq = square(x);
+    return x * (-35.0/4 + 7*xsq - square(xsq))*exp(-xsq)/denom;
+  }
+
+  KOKKOS_INLINE_FUNCTION
+  Real laplacian(const Real& x) const {
+    const Real xsq = square(x);
+    return (17.5 - 14*xsq + 2*square(xsq))*exp(-xsq)/denom;
+  }
+};
+
+struct Line8thOrder {
+  Real eps;
+  Real epssq;
+  Real denom;
+  static constexpr Int order = 8;
+
+  KOKKOS_INLINE_FUNCTION
+  explicit Line8thOrder(const Real eps) : eps(eps), epssq(square(eps)),
+    denom(sqrt(constants::PI)) {}
+
+  KOKKOS_INLINE_FUNCTION
+  Real blob(const Real& x) const {
+    const Real xsq = square(x);
+    const Real x4th = square(xsq);
+    return ( 35.0/16 - 35*xsq/8 + 7*x4th/4 - xsq*x4th/6) * exp(-square(x))/denom;
+  }
+
+  KOKKOS_INLINE_FUNCTION
+  Real scaled_blob(const Real& x) const {
+    return impl::line_scaled_blob(x, *this);
+  }
+
+  KOKKOS_INLINE_FUNCTION
+  Real x_derivative(const Real& x) const {
+    const Real xsq = square(x);
+    const Real x4th = square(xsq);
+    return x * (-105.0/4 + 63*xsq/4 -4.5*x4th + xsq*x4th/6)*exp(-xsq)/denom;
+  }
+
+  KOKKOS_INLINE_FUNCTION
+  Real laplacian(const Real& x) const {
+    const Real xsq = square(x);
+    const Real x4th = square(xsq);
+    return (105.0/4 - 63.0/2*xsq + 9*x4th - 2*xsq*x4th/3)*exp(-xsq)/denom;
+  }
+};
+
+template <typename KernelType>
+struct LineXDerivativeReducer {
+  using crd_view = scalar_view_type;
+  using vec_view = scalar_view_type;
+  using value_type = Real;
+  using kernel_type = KernelType;
+  using kernel_order = KernelType::order;
+  using view_type = scalar_view_type;
+  static constexpr Int ndim = 1;
+  static constexpr Int order = 1;
+  scalar_view_type x;
+  scalar_view_type f_values;
+  scalar_view_type length;
+  kernel_type kernels;
+  Index i;
+
+  KOKKOS_INLINE_FUNCTION
+  LineXDerivativeReducer(const scalar_view_type x, const scalar_view_type f, const KernelType& kernels,
+    const scalar_view_type l, const Index i) :
+    x(x), f_values(f), length(l), kernels(kernels), i(i) {}
+
+  KOKKOS_INLINE_FUNCTION
+  void operator() (const Index& j, Real& dfdx) const {
+     const Real xij = (x(i) - x(j))/kernels.eps;
+     dfdx += (f_values(i) + f_values(j)) * kernels.x_derivative(xij) / kernels.eps * length(j) / kernels.eps;
+  }
+};
+
+template <typename KernelType>
+struct Line2ndDerivativeReducer {
+  using crd_view = scalar_view_type;
+  using vec_view = scalar_view_type;
+  using value_type = Real;
+  using kernel_type = KernelType;
+  using kernel_order = KernelType::order;
+  using view_type = scalar_view_type;
+  static constexpr Int ndim = 1;
+  static constexpr Int order = 2;
+  scalar_view_type x;
+  scalar_view_type f_values;
+  scalar_view_type length;
+  kernel_type kernels;
+  Index i;
+
+  KOKKOS_INLINE_FUNCTION
+  Line2ndDerivativeReducer(const scalar_view_type x, const scalar_view_type f, const KernelType& kernels,
+    const scalar_view_type l, const Index i) :
+    x(x), f_values(f), length(l), kernels(kernels), i(i) {}
+
+  KOKKOS_INLINE_FUNCTION
+  void operator() (const Index& j, Real& df2dx2) const {
+     const Real xij = (x(i) - x(j))/kernels.eps;
+     df2dx2 += (f_values(j) - f_values(i)) * kernels.laplacian(xij) / kernels.eps * length(j) / kernels.epssq;
+  }
+};
+
+template <typename ReducerType>
+struct LineDirectSum {
+  using kernel_type = typename ReducerType::kernel_type;
+  using value_type = typename ReducerType::value_type;
+  using view_type = typename ReducerType::view_type;
+  view_type output_view;
+  scalar_view_type x;
+  scalar_view_type f;
+  kernel_type kernels;
+  scalar_view_type length;
+  Index n_src;
+
+  LineDirectSum(scalar_view_type output_view, const scalar_view_type x,
+    const scalar_view_type f, const kernel_type& kernels, const scalar_view_type length,
+    const Index n) :
+    output_view(output_view),
+    x(x), f(f),
+    kernels(kernels),
+    length(length),
+    n_src(n) {}
+
+  KOKKOS_INLINE_FUNCTION
+  void operator() (const member_type& thread_team) const {
+    const Index i = thread_team.league_rank();
+    Real output_value;
+    Kokkos::parallel_reduce(Kokkos::TeamVectorRange(thread_team, n_src),
+      ReducerType(x, f, kernels, length, i), output_value);
+    output_view(i) = output_value;
+  }
+};
+
+} // namespace colloc
+} // namespace Lpm
+#endif
--- src/lpm_regularized_kernels_2d.hpp
+++ src/lpm_regularized_kernels_2d.hpp
@@ -8,11 +8,41 @@
 #include "util/lpm_tuple.hpp"
 
 namespace Lpm {
+namespace colloc {
+
+namespace impl {
+template <typename XType, typename KernelType> KOKKOS_INLINE_FUNCTION
+Kokkos::Tuple<Real,6> plane_velocity_kernel_impl(const XType& x, const KernelType& kernels) {
+  const Real xscaled[2] = {x[0]/kernels.eps, x[1]/kernels.eps};
+  const Real one_over_rsq = FloatingPoint<Real>::safe_denominator(PlaneGeometry::norm2(x));
+  const Real q = kernels.blobq(xscaled);
+  const Real ucoeff = q * one_over_rsq / (2*constants::PI);
+  const Real grad_coeff = -(kernels.scaled_blob(x) - q * one_over_rsq /constants::PI) * one_over_rsq;
+  const Real grad_diag = q * one_over_rsq / (2*constants::PI);
+  Kokkos::Tuple<Real,6> u;
+  u[0] = -x[0] * ucoeff;
+  u[1] = -x[1] * ucoeff;
+  u[2] = grad_coeff * square(x[0]) - grad_diag;
+  u[3] = grad_coeff * x[0] * x[1];
+  u[4] = grad_coeff * x[1] * x[0];
+  u[5] = grad_coeff * square(x[1]) - grad_diag;
+  return u;
+}
+
+template <typename XType, typename KernelType> KOKKOS_INLINE_FUNCTION
+Real plane_scaled_blob_impl(const XType& x, const KernelType& kernels) {
+  const Real xscaled[2] = {x[0]/kernels.eps, x[1]/kernels.eps};
+  return kernels.blob(xscaled)/kernels.epssq;
+}
+
+} // namespace impl
 
 /**  Each kernel set provides "blob" functions for interpolation,
   velocity kernels, and derivatives such as the laplacian, all
   with the same order.
 */
+
+
 
 /** 2nd order kernels for the plane
 */
@@ -36,8 +66,7 @@ struct Plane2ndOrder {
 
   template <typename XType> KOKKOS_INLINE_FUNCTION
    Real scaled_blob(const XType& x) const {
-    const Real xscaled[2] = {x[0]/eps, x[1]/eps};
-    return blob(xscaled)/epssq;
+    return impl::plane_scaled_blob_impl(x, *this);
   }
 
   template <typename XType> KOKKOS_INLINE_FUNCTION
@@ -57,20 +86,7 @@ struct Plane2ndOrder {
   */
   template <typename XType> KOKKOS_INLINE_FUNCTION
   Kokkos::Tuple<Real,6> velocity(const XType& x) const {
-    const Real xscaled[2] = {x[0]/eps, x[1]/eps};
-    const Real one_over_rsq = FloatingPoint<Real>::safe_denominator(PlaneGeometry::norm2(x));
-    const Real q = blobq(xscaled);
-    const Real ucoeff = q * one_over_rsq / (2*constants::PI);
-    const Real grad_coeff = (-scaled_blob(x) + q * one_over_rsq / constants::PI) * one_over_rsq;
-    const Real grad_diag = - q * one_over_rsq / (2*constants::PI);
-    Kokkos::Tuple<Real,6> v;
-    v[0] = -x[0] * ucoeff ;
-    v[1] = -x[1] * ucoeff ;
-    v[2] = grad_coeff * square(x[0]) + grad_diag;
-    v[3] = grad_coeff * x[0] * x[1];
-    v[4] = grad_coeff * x[1] * x[0];
-    v[5] = grad_coeff * square(x[1]) + grad_diag;
-    return v;
+    return impl::plane_velocity_kernel_impl(x, *this);
   }
 
   template <typename XType> KOKKOS_INLINE_FUNCTION
@@ -140,10 +156,7 @@ struct Plane4thOrder {
 
   template <typename XType> KOKKOS_INLINE_FUNCTION
   Real scaled_blob(const XType& x) const {
-    const Real xscaled[2] = {x[0]/eps, x[1]/eps};
-    const Real rsq = PlaneGeometry::norm2(xscaled);
-    const Real coeff = ( 2 - rsq ) / (constants::PI * epssq);
-    return coeff*exp(-rsq);
+    return impl::plane_scaled_blob_impl(x, *this);
   }
 
   /** Returns a packed reduction with velocity and the velocity gradient tensor.
@@ -157,21 +170,7 @@ struct Plane4thOrder {
   */
   template <typename XType> KOKKOS_INLINE_FUNCTION
   Kokkos::Tuple<Real,6> velocity(const XType& x) const {
-    const Real xscaled[2] = {x[0]/eps, x[1]/eps};
-    const Real rsq = PlaneGeometry::norm2(xscaled);
-    const Real one_over_rsq = FloatingPoint<Real>::safe_denominator(PlaneGeometry::norm2(x));
-    const Real q = blobq(xscaled);
-    const Real ucoeff = q * one_over_rsq / (2*constants::PI);
-    const Real grad_coeff = (-scaled_blob(x) + q * one_over_rsq / constants::PI) * one_over_rsq;
-    const Real grad_diag = - q * one_over_rsq / (2*constants::PI);
-    Kokkos::Tuple<Real,6> v;
-    v[0] = -x[0] * ucoeff ;
-    v[1] = -x[1] * ucoeff ;
-    v[2] = grad_coeff * square(x[0]) + grad_diag;
-    v[3] = grad_coeff * x[0] * x[1];
-    v[4] = grad_coeff * x[1] * x[0];
-    v[5] = grad_coeff * square(x[1]) + grad_diag;
-    return v;
+    return impl::plane_velocity_kernel_impl(x, *this);
   }
 
   template <typename XType> KOKKOS_INLINE_FUNCTION
@@ -238,10 +237,7 @@ struct Plane6thOrder {
 
   template <typename XType> KOKKOS_INLINE_FUNCTION
    Real scaled_blob(const XType& x) const {
-    const Real xscaled[2] = {x[0]/eps, x[1]/eps};
-    const Real rsq = PlaneGeometry::norm2(xscaled);
-    const Real coeff = ( 3 - 3*rsq + 0.5*square(rsq) ) / (constants::PI * epssq);
-    return coeff*exp(-rsq);
+    return impl::plane_scaled_blob_impl(x, *this);
   }
 
   template <typename XType> KOKKOS_INLINE_FUNCTION
@@ -288,21 +284,7 @@ struct Plane6thOrder {
   */
   template <typename XType> KOKKOS_INLINE_FUNCTION
   Kokkos::Tuple<Real,6> velocity(const XType& x) const {
-    const Real xscaled[2] = {x[0]/eps, x[1]/eps};
-    const Real rsq = PlaneGeometry::norm2(xscaled);
-    const Real one_over_rsq = FloatingPoint<Real>::safe_denominator(PlaneGeometry::norm2(x));
-    const Real q = blobq(xscaled);
-    const Real ucoeff = q * one_over_rsq / (2*constants::PI);
-    const Real grad_coeff = (-scaled_blob(x) + q * one_over_rsq / constants::PI) * one_over_rsq;
-    const Real grad_diag = - q * one_over_rsq / (2*constants::PI);
-    Kokkos::Tuple<Real,6> v;
-    v[0] = -x[0] * ucoeff ;
-    v[1] = -x[1] * ucoeff ;
-    v[2] = grad_coeff * square(x[0]) + grad_diag;
-    v[3] = grad_coeff * x[0] * x[1];
-    v[4] = grad_coeff * x[1] * x[0];
-    v[5] = grad_coeff * square(x[1]) + grad_diag;
-    return v;
+    return impl::plane_velocity_kernel_impl(x, *this);
   }
 
   template <typename XType> KOKKOS_INLINE_FUNCTION
@@ -344,15 +326,15 @@ struct Plane8thOrder {
 
   template <typename XType> KOKKOS_INLINE_FUNCTION
    Real scaled_blob(const XType& x) const {
-    const Real xscaled[2] = {x[0]/eps, x[1]/eps};
-    return blob(xscaled) / epssq;
+    return impl::plane_scaled_blob_impl(x, *this);
   }
 
   template <typename XType> KOKKOS_INLINE_FUNCTION
   Real blobq(const XType& x) const {
     const Real rsq = PlaneGeometry::norm2(x);
     const Real r4th = square(rsq);
-    return 1 - (1 - 3*rsq + 1.5*r4th - rsq*r4th/6)*exp(-rsq);
+    const Real coeff = -1 + 3*rsq - 1.5*r4th + r4th*rsq/6;
+    return 1 + coeff*exp(-rsq);
   }
 
   template <typename XType> KOKKOS_INLINE_FUNCTION
@@ -393,20 +375,7 @@ struct Plane8thOrder {
   */
   template <typename XType> KOKKOS_INLINE_FUNCTION
   Kokkos::Tuple<Real,6> velocity(const XType& x) const {
-    const Real xscaled[2] = {x[0]/eps, x[1]/eps};
-    const Real one_over_rsq = FloatingPoint<Real>::safe_denominator(PlaneGeometry::norm2(x));
-    const Real q = blobq(xscaled);
-    const Real ucoeff = q * one_over_rsq / (2*constants::PI);
-    const Real grad_coeff = (-scaled_blob(x) + q * one_over_rsq / constants::PI) * one_over_rsq;
-    const Real grad_diag = - q * one_over_rsq / (2*constants::PI);
-    Kokkos::Tuple<Real,6> v;
-    v[0] = -x[0] * ucoeff ;
-    v[1] = -x[1] * ucoeff ;
-    v[2] = grad_coeff * square(x[0]) + grad_diag;
-    v[3] = grad_coeff * x[0] * x[1];
-    v[4] = grad_coeff * x[1] * x[0];
-    v[5] = grad_coeff * square(x[1]) + grad_diag;
-    return v;
+    return impl::plane_velocity_kernel_impl(x, *this);
   }
 
   template <typename XType> KOKKOS_INLINE_FUNCTION
@@ -576,53 +545,6 @@ struct PlaneGradientReducer {
 };
 
 template <typename KernelType>
-struct PlaneOneSidedInteriorGradientReducer {
-  using crd_view = PlaneGeometry::crd_view_type;
-  using vec_view = PlaneGeometry::vec_view_type;
-  using value_type = Kokkos::Tuple<Real,2>;
-  using kernel_type = KernelType;
-  using view_type = vec_view;
-  static constexpr Int ndim = 2;
-  crd_view tgt_x;
-  scalar_view_type tgt_values;
-  crd_view src_x;
-  KernelType kernels;
-  scalar_view_type src_values;
-  scalar_view_type area;
-  mask_view_type src_mask;
-  Index i;
-
-  KOKKOS_INLINE_FUNCTION
-  PlaneOneSidedInteriorGradientReducer(const crd_view tgt_x, const scalar_view_type tgt_values,
-    const crd_view src_x, const scalar_view_type src_values,
-    const KernelType& kernels, const scalar_view_type area,
-    const mask_view_type src_mask, const Index i) :
-    tgt_x(tgt_x),
-    tgt_values(tgt_values),
-    src_x(src_x),
-    src_values(src_values),
-    kernels(kernels),
-    area(area),
-    src_mask(src_mask),
-    i(i) {}
-
-  KOKKOS_INLINE_FUNCTION
-  void operator() (const Index& j, value_type& grad) const {
-    if (!src_mask(j)) {
-      const auto x_tgt = Kokkos::subview(tgt_x, i, Kokkos::ALL);
-      const auto x_src = Kokkos::subview(src_x, j, Kokkos::ALL);
-      const bool is_interior = PlaneGeometry::norm2(x_tgt) - PlaneGeometry::norm2(x_src) > 0;
-      if (is_interior) {
-        const Real xij[2] = {x_tgt[0] - x_src[0], x_tgt[1] - x_src[1]};
-        const Real vij = area(j) * (tgt_values(j) + src_values(i));
-        grad[0] += vij * kernels.left_x0_derivative(xij) / kernels.eps;
-        grad[1] += vij * kernels.left_x1_derivative(xij) / kernels.eps;
-      }
-    }
-  }
-};
-
-template <typename KernelType>
 struct PlaneLaplacianReducer {
   using crd_view = PlaneGeometry::crd_view_type;
   using vec_view = PlaneGeometry::vec_view_type;
@@ -771,5 +693,107 @@ struct VelocityDirectSum {
   }
 };
 
+template <typename KernelType>
+struct PlaneInteriorGradientReducer {
+  using crd_view = PlaneGeometry::crd_view_type;
+  using vec_view = PlaneGeometry::vec_view_type;
+  using value_type = Kokkos::Tuple<Real,2>;
+  using kernel_type = KernelType;
+  using view_type = vec_view;
+  static constexpr Int ndim = 2;
+  crd_view tgt_x;
+  scalar_view_type tgt_values;
+  crd_view src_x;
+  KernelType kernels;
+  scalar_view_type src_values;
+  scalar_view_type area;
+  mask_view_type src_mask;
+  Index i;
+  bool tgt_in_boundary_zone;
+
+  KOKKOS_INLINE_FUNCTION
+  PlaneInteriorGradientReducer(const crd_view tgt_x, const scalar_view_type tgt_values,
+    const crd_view src_x, const scalar_view_type src_values,
+    const KernelType& kernels, const scalar_view_type area,
+    const mask_view_type src_mask, const Index i, const bool in_boundary) :
+    tgt_x(tgt_x),
+    tgt_values(tgt_values),
+    src_x(src_x),
+    src_values(src_values),
+    kernels(kernels),
+    area(area),
+    src_mask(src_mask),
+    i(i),
+    tgt_in_boundary_zone(in_boundary) {}
+
+  KOKKOS_INLINE_FUNCTION
+  void operator() (const Index& j, value_type& grad) const {
+    if (!src_mask(j)) {
+      const auto x_tgt = Kokkos::subview(tgt_x, i, Kokkos::ALL);
+      const auto x_src = Kokkos::subview(src_x, j, Kokkos::ALL);
+      const Real xij[2] = {x_tgt[0] - x_src[0], x_tgt[1] - x_src[1]};
+      const Real vij = area(j) * (tgt_values(j) + src_values(i));
+      if (tgt_in_boundary_zone) {
+        const bool is_interior = PlaneGeometry::norm2(x_tgt) > PlaneGeometry::norm2(x_src);
+        if (is_interior) {
+          grad[0] += vij * kernels.left_x0_derivative(xij) / kernels.eps;
+          grad[1] += vij * kernels.left_x1_derivative(xij) / kernels.eps;
+        }
+      }
+      else {
+        grad[0] += vij * kernels.x0_derivative(xij) / kernels.eps;
+        grad[1] += vij * kernels.x1_derivative(xij) / kernels.eps;
+      }
+    }
+  }
+};
+
+template <typename ReducerType>
+struct InteriorRadialGradientDirectSum {
+  using kernel_type = typename ReducerType::kernel_type;
+  using value_type = typename ReducerType::value_type;
+  using view_type = typename ReducerType::view_type;
+  using crd_view = PlaneGeometry::crd_view_type;
+  using vec_view = PlaneGeometry::vec_view_type;
+  view_type output_view;
+  scalar_view_type tgt_f;
+  crd_view tgt_x;
+  crd_view src_x;
+  scalar_view_type src_f;
+  kernel_type kernels;
+  scalar_view_type area;
+  mask_view_type src_mask;
+  Index n_src;
+  Real boundary_radius;
+
+  InteriorRadialGradientDirectSum(view_type output_view, const crd_view tx, const scalar_view_type tf,
+    const crd_view sx, const scalar_view_type f,
+    const kernel_type& kernels,
+    const scalar_view_type area, const mask_view_type mask, const Index n, const Real boundary_radius) :
+    output_view(output_view),
+    tgt_x(tx),
+    tgt_f(tf),
+    src_x(sx),
+    src_f(f),
+    kernels(kernels),
+    area(area),
+    src_mask(mask),
+    n_src(n),
+    boundary_radius(boundary_radius) {}
+
+  KOKKOS_INLINE_FUNCTION
+  void operator() (const member_type& thread_team) const {
+    const Index i = thread_team.league_rank();
+    const auto x_i = Kokkos::subview(tgt_x, i, Kokkos::ALL);
+    const bool in_boundary = PlaneGeometry::norm2(x_i) > square(boundary_radius);
+    value_type f_sum;
+    Kokkos::parallel_reduce(Kokkos::TeamVectorRange(thread_team, n_src),
+      PlaneInteriorGradientReducer(tgt_x, tgt_f, src_x, src_f, kernels, area, src_mask, i, in_boundary), f_sum);
+    output_view(i,0) = f_sum[0];
+    output_view(i,1) = f_sum[1];
+  }
+};
+
+} // namespace colloc
 } // namespace Lpm
 #endif
