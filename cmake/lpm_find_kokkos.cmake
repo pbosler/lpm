cc23b157ac5a53d6b15c41242be82e6ca368b2a4
Peter Bosler <pabosle.sandia.gov>
2025-09-08 11:25:47 -0600

    restoring cmake logic to determine if cuda is enabled.

--- cmake/lpm_find_kokkos.cmake
+++ cmake/lpm_find_kokkos.cmake
@@ -3,6 +3,14 @@ message(STATUS "LPM: looking for KokkosKernels at ${KokkosKernels_DIR}")
 message(STATUS "LPM: looking for Compadre at ${Compadre_DIR}")
 
 find_package(Kokkos REQUIRED HINTS ${Kokkos_DIR})
+printvar(Kokkos_DEVICES)
+
+list(JOIN Kokkos_DEVICES ", " DEVICE_STR)
+message(STATUS "LPM: Found Kokkos devices ${DEVICE_STR}")
+if (${DEVICE_STR} MATCHES "CUDA")
+  set(LPM_USE_CUDA TRUE CACHE BOOL "")
+  message(STATUS "LPM: setting LPM_USE_CUDA = ${LPM_USE_CUDA}")
+endif()
 
 find_package(KokkosKernels REQUIRED HINTS ${KokkosKernels_DIR})
 
